99c8c1a168641a035cdaf6e7d30c6bac697feef8
Usama Bashir <usama.bashirb1@gmail.com>
2022-07-30 11:35:32 +0500

    Changes

--- middlewares/check-auth.js
+++ middlewares/check-auth.js
@@ -11,7 +11,7 @@ module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
         if (!token) {
-            throw new Error('Authentication failed');
+            throw new Error('Authentication is failed');
         }
         const decodedToken = jwt.verify(token, process.env.JWT_KEY);
         req.userData = { userId: decodedToken.userId }
